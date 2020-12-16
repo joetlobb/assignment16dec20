186c6a97564d02df1e514ec6f01f2616c00fe7ea
Joetlobb <joetlobb@Joetlobbs-MacBook-Pro.local>
2020-12-17 01:05:11 +0700

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'refinitiv';
-}
+
+  _input: any;
+  selectedOption = 'isPrime';
+  result: boolean;
+
+  calculateResult() {
+    if (this._input) {
+      let num = this._input;
+      if (num === '-') {
+        return this._input = 1;
+      } else if (num % 1 !== 0) {
+        num = Math.round(num);
+      } else if (num < 0) {
+        num = 1;
+      };
+      this._input = num;
+
+      if (this.selectedOption === 'isPrime') {
+        for (let i = 2; i < num; i++) {
+          if (num % i === 0) {
+            return this.result = false;
+          };
+        };
+        return this.result = num > 1;
+      } else {
+        num = +num;
+        let fibSeq = [0, 1], fibSeqL: number;
+        for (let i = 1; i <= num; i = (fibSeq[fibSeqL - 1] + fibSeq[fibSeqL])) {
+          fibSeq.push(i);
+          fibSeqL = fibSeq.length - 1;
+        }
+        return this.result = fibSeq[fibSeqL] === num;
+      };
+    } else if (!this._input && this.selectedOption !== 'isPrime') {
+      this._input = 0;
+      return this.result = true;
+    } else return this.result = false;
+  };
+
+};
